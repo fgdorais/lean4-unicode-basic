316625dc161913b9c9288671c760927efb166454
F. G. Dorais <fgdorais@gmail.com>
2023-03-15 11:42:17 -0400

    feat: update script for UCD

--- lakefile.lean
+++ lakefile.lean
@@ -9,6 +9,24 @@ open Lake DSL
 package «lean4-unicode-basic»
 
 @[default_target]
-lean_lib UnicodeBasic
+lean_lib UnicodeBasic {
+  precompileModules := true
+}
 
 lean_exe UnicodeTool
+
+script UpdateUCD := do
+  let dir : FilePath := "./data"
+  for file in ["UnicodeData.txt"] do
+    IO.println s!"Updating {file}"
+    let url := "https://www.unicode.org/Public/UCD/latest/ucd/" ++ file
+
+    let mut args := #["-sSO", "--remote-time"]
+    if ← System.FilePath.pathExists <| dir/file then
+      args := args ++ #["-z", file]
+    let _ ← IO.Process.run {
+      cmd := "curl"
+      args := args.push url
+      cwd := some dir
+    }
+  return 0
