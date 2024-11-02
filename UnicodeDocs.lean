339a6205a1134b390ec1d343a7114bc8a2a9abbb
François G. Dorais <fgdorais@gmail.com>
2024-11-01 23:22:51 -0400

    feat: use subdirectory to generate docs (#45)

---
+++ UnicodeDocs.lean
@@ -0,0 +1,9 @@
+/-
+Copyright © 2024 François G. Dorais. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
+import UnicodeBasic
+import UnicodeData
+
+/-! Imports `UnicodeBasic` and `UnicodeData` to generate documentation. -/
