88b80ee19b0a058a93ebb2f8cb21c68ab683ebdb
F. G. Dorais <fgdorais@gmail.com>
2024-11-01 21:26:59 -0400

    chore: make lakefile aware of reservoir

--- lakefile.lean
+++ lakefile.lean
@@ -6,14 +6,16 @@ Released under Apache 2.0 license as described in the file LICENSE.
 import Lake
 open Lake DSL
 
-package UnicodeBasic
+package «lean4-unicode-basic» where
+  version := v!"1.0.3"
+  description := "Basic Unicode support for Lean 4"
+  keywords := #["unicode"]
+  reservoir := true
 
 @[default_target]
 lean_lib UnicodeBasic
 
-lean_lib UnicodeData {
-  precompileModules := true
-}
+lean_lib UnicodeData
 
 lean_exe lookup
 
@@ -22,7 +24,7 @@ lean_exe makeTables
 @[test_driver]
 lean_exe testTables
 
--- Download datafile from the Unicode Character Database (UCD)
+/-- Download datafile from the Unicode Character Database (UCD) -/
 script downloadUCD (args) do
   let dir : System.FilePath := "./data"
   let url := "https://www.unicode.org/Public/UCD/latest/ucd/"
@@ -34,5 +36,5 @@ script downloadUCD (args) do
     | .error _ _ => IO.println "Failed!"; err := err + 1
   return err
 
--- Update data files from the Unicode Character Database (UCD)
+/-- Update data files from the Unicode Character Database (UCD) -/
 script updateUCD do downloadUCD ["ReadMe.txt", "UnicodeData.txt", "PropList.txt"]
