f90e00628973fd06f55ca507286610cc5313d982
François G. Dorais <fgdorais@gmail.com>
2025-12-21 15:13:07 -0500

    chore: modulize `UnicodeData`

--- UnicodeData/PropList.lean
+++ UnicodeData/PropList.lean
@@ -1,10 +1,11 @@
 /-
-Copyright © 2023-2024 François G. Dorais. All rights reserved.
+Copyright © 2023-2025 François G. Dorais. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
-import UnicodeBasic.Types
-import UnicodeBasic.CharacterDatabase
+module
+public import UnicodeBasic.Types
+import all UnicodeBasic.CharacterDatabase
 
 namespace Unicode
 
