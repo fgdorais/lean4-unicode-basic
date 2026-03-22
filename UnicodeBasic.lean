e212c4f2ba4505bf5762a7c5959a3984d3b4cbc9
F. G. Dorais <fgdorais@gmail.com>
2026-03-22 16:04:57 -0400

    fix: docstring edit

--- UnicodeBasic.lean
+++ UnicodeBasic.lean
@@ -49,7 +49,7 @@ namespace Unicode
 /-- Get character name
 
   When the Unicode property `Name` is empty, a unique code label is returned
-  as described in Unicode Standard, section 4.8. These labels start with
+  as recommended in Unicode Standard, section 4.8. These labels start with
   `'<'` (U+003C) and end with `'>'` (U+003E) so they are distinguishable from
   actual name values.
 
